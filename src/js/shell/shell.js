35a9bd2c3d5da370f4277d997354cc2fc168e68f
Ellie <ellie@magnon.net>
2017-06-09 13:56:17 +0200

    Update shell with state links

--- src/js/shell/shell.js
+++ src/js/shell/shell.js
@@ -1,7 +1,9 @@
+import Navigator from "../navigator/navigator.js";
+
 const navigation = [
     ["Home", "/"],
-    ["Games", "/games"],
-    ["About Us", "/about"],
+    ["Games", { href: "/", value: { scroll: "#games" }}],
+    ["About Us", { href: "/", value: { scroll: "#about" }}],
     ["Blog", "https://blog.magnon.net"]
 ];
 
@@ -9,7 +11,15 @@ export function buildShell() {
     const navigationElements = navigation.map(nav => {
         const a = document.createElement("a");
         a.textContent = nav[0];
-        a.href = nav[1];
+
+        const link = nav[1];
+        if (typeof link === "string") {
+            a.href = link;
+        } else {
+            a.onclick = () => {
+                Navigator.goToPage(link.href, false, link.value);
+            };
+        }
         a.slot = "nav";
         return a;
     });
