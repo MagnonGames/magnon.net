ea1711762b94ebd7b3843f9225e5c0030b469c30
Ellie <ellie@magnon.net>
2017-06-10 00:39:18 +0200

    Tweak about animations

--- src/pages/home/about.js
+++ src/pages/home/about.js
@@ -9,21 +9,17 @@ export default () => {
             translateY: [-100, 0],
             opacity: [0, 1],
             easing: "easeOutQuad",
-            duration: 1000
+            duration: 500
         });
 
         anime({
             targets: "#members > magnon-image",
-            translateY: [250, 0],
-            scale: [0.5, 1],
             opacity: {
                 value: [0, 1],
                 easing: "linear"
             },
             duration: 500,
-            elasticity: 500,
-            easing: "easeOutElastic",
-            delay: (n, i) => 1250 + i * 400
+            delay: (n, i) => 650 + i * 200
         });
     });
 };
