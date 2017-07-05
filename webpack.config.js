81e245cf501af146481abb1bacdd88d1784fac3d
Ellie <ellie@magnon.net>
2017-07-05 22:33:22 +0200

    Improve image minification

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,18 @@ const productionPlugins = (() => {
         new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 10000 })
     ];
 })();
+const productionImageLoaderOptions = (() => {
+    if (!production) return {};
+    return {
+        optipng: {
+            optimizationLevel: 7
+        },
+        pngquant: {
+            quality: "65-90",
+            speed: 4
+        }
+    };
+})();
 
 const hash = production ? "-[hash]" : "";
 
@@ -47,7 +59,13 @@ module.exports = new Promise((resolve, reject) => {
                     { test: /src\/.*?\.html$/, loader: "html-loader" },
                     {
                         test: /\.(jpe?g|png|gif|svg)$/,
-                        loaders: ["file-loader", "image-webpack-loader"]
+                        loaders: [
+                            "file-loader",
+                            {
+                                loader: "image-webpack-loader",
+                                query: productionImageLoaderOptions
+                            }
+                        ]
                     },
                     { test: /\.yml$/, loaders: ["json-loader", "yaml-loader"] }
                 ]
